67e2f589683de772ea1ad942cfc98e70d147f4d6
Brian Chan <i@b123400.net>
2017-07-15 01:00:36 +0100

    Add example for purescript-pux-form

--- src/App/State.purs
+++ src/App/State.purs
@@ -3,9 +3,24 @@ module App.State where
 import App.Config (config)
 import App.Routes (Route, match)
 import Data.Newtype (class Newtype)
+import Data.Show (class Show)
+import Data.Eq (class Eq)
+
+import Data.Lens (Lens', lens)
+import Pux.Form.Render (class MultipleChoice)
+
+data Gender = Male | Female | Secret
 
 newtype State = State
   { title :: String
+  , name :: String
+  , biography :: String
+  , age :: Int
+  , alive :: Boolean
+  , password :: String
+  , avatar :: String
+  , interestRate :: Number
+  , gender :: Gender
   , route :: Route
   , loaded :: Boolean
   }
@@ -15,6 +30,51 @@ derive instance newtypeState :: Newtype State _
 init :: String -> State
 init url = State
   { title: config.title
+  , name: "hello"
+  , biography: "yo"
+  , age: 1
+  , alive: true
+  , password: "hey"
+  , avatar: ""
+  , interestRate: 0.2
+  , gender: Male
   , route: match url
   , loaded: false
   }
+
+title :: Lens' State String
+title = lens (\(State s)-> s.title) (\(State s) val-> State s { title = val })
+
+name :: Lens' State String
+name = lens (\(State s)-> s.name) (\(State s) val-> State s { name = val })
+
+biography :: Lens' State String
+biography = lens (\(State s)-> s.biography) (\(State s) val-> State s { biography = val })
+
+age :: Lens' State Int
+age = lens (\(State s)-> s.age) (\(State s) val-> State s { age = val })
+
+alive :: Lens' State Boolean
+alive = lens (\(State s)-> s.alive) (\(State s) val-> State s { alive = val })
+
+password :: Lens' State String
+password = lens (\(State s)-> s.password) (\(State s) val-> State s { password = val })
+
+avatar :: Lens' State String
+avatar = lens (\(State s)-> s.avatar) (\(State s) val-> State s { avatar = val })
+
+interestRate :: Lens' State Number
+interestRate = lens (\(State s)-> s.interestRate) (\(State s) val-> State s { interestRate = val })
+
+gender :: Lens' State Gender
+gender = lens (\(State s)-> s.gender) (\(State s) val-> State s { gender = val })
+
+instance multipleChoiceGender :: MultipleChoice Gender where
+  choices = [Male, Female, Secret]
+
+derive instance eqGender :: Eq Gender
+
+instance showGender :: Show Gender where
+  show Male = "Male"
+  show Female = "Female"
+  show Secret = "Secret"
--- src/App/View/Homepage.purs
+++ src/App/View/Homepage.purs
@@ -1,17 +1,38 @@
 module App.View.Homepage where
 
-import App.Events (Event)
-import App.State (State)
-import Control.Bind (discard)
-import Data.Function (($))
+import Prelude hiding (div)
+import App.Events (Event(..))
+import App.State (State(..), Gender(..), title, name, biography, age, alive, password, avatar, interestRate, gender)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (a, div, h1)
-import Text.Smolder.HTML.Attributes (href, className)
-import Text.Smolder.Markup ((!), text)
+import Text.Smolder.HTML (div, p, b)
+import Text.Smolder.Markup (text)
+
+import Pux.Form (form, (.+), (.|), (.\), (./), (.?))
+import Pux.Form.Render (asTextArea, asPassword, asFile, asRange, asRangeNum, asDropdown, asDropdown')
 
 view :: State -> HTML Event
-view s =
+view (s@State s') =
   div do
-    h1 $ text "Pux"
-    a ! className "guide" ! href "https://www.purescript-pux.org/" $ text "Guide"
-    a ! className "github" ! href "https://github.com/alexmingoia/purescript-pux/" $ text "GitHub"
+    p $ text ("Title: "        <>      s'.title)
+    p $ text ("Name: "         <>      s'.name)
+    p $ text ("Age: "          <> show s'.age)
+    p $ text ("Biography: "    <>      s'.biography)
+    p $ text ("Password: "     <>      s'.password)
+    p $ text ("Alive? "        <> show s'.alive)
+    p $ text ("Avatar: "       <>      s'.avatar)
+    p $ text ("Interest rate:" <> show s'.interestRate)
+    p $ text ("Your gender:"   <> show s'.gender)
+    form s fields Replace
+  where fields = title                           .\ "Enter title"      .? ((/=) "hi")
+              .+ age                              .\ "Enter age"       .? (_ < 100)
+              .+ (asRange age 10 100 2)            .\ "Input age as Range"
+              .+ name                               .| (\d-> (b $ text "before") *> d *> (b $ text "after"))
+              .+ name                              ./ (b $ text "Enter name")
+              .+ (asPassword password)             .\ "Enter your password"
+              .+ (asTextArea biography)             .\ "Enter biography in text area"
+              .+ alive                               .\ "Are you alive?"
+              .+ (asFile avatar)                      .\ "Choose your avatar"
+              .+ interestRate                          .\ "Interest rate"
+              .+ (asRangeNum interestRate 0.1 10.0 0.2) .\ "Interest rate"
+              .+ (asDropdown gender)                     .\ "Your gender?"
+              .+ (asDropdown' gender [Male, Female])      .\ "Your gender?"
